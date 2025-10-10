e49076616dfd1a1e51baaecc2e5657bada766fed
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-10-10 23:37:21 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,21 @@
-const CACHE_NAME = "ribcage-log-v2";
+const CACHE_NAME = "ribcage-log-v1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./offline.html",
-  "./images/deadbug-1.jpg",
-  "./images/bounce-1.jpg",
-  "./images/hipdrift-1.jpg",
-  "./images/press-1.jpg",
-  "./images/mblead-1.jpg",
-  "./images/wblead-1.jpg"
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
 // インストール
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// フェッチ（オフライン時フォールバック）
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then((response) => {
-      return response || caches.match("./offline.html");
-    }))
-  );
-});
-
-// アクティベート（古いキャッシュ削除）
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+// フェッチ
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
